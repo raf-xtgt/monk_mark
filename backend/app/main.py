9f46978caba5feba3665a3d11b992fe9c5aa8050
raf-xtgt <rafaquat9905@gmail.com>
2026-01-24 08:03:34 +0600

    setup web service application

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI(title="Monk Mark Web Application Service")
+
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+url_prefix = "/api/ppl"
+
+@app.get("/")
+def read_root():
+    return {"message": "Monk mark api running"}
+
