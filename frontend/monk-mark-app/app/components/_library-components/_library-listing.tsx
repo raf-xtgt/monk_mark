2cf6c9f72f7903491b4eab477a2ea3b1079d8eb8
raf-xtgt <rafaquat9905@gmail.com>
2026-01-26 20:37:38 +0600

    skeleton code for library listing

---
+++ frontend/monk-mark-app/app/components/_library-components/_library-listing.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { LibraryService } from '../../_services/library-service';
+
+
+const LibraryListing: React.FC = () => {
+  
+  return (
+    <View>
+        <Text>Library listing</Text>
+
+    </View>
+  );
+};
+
+export default LibraryListing;
--- frontend/monk-mark-app/app/pages/library/library-container.tsx
+++ frontend/monk-mark-app/app/pages/library/library-container.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import LibraryListing from '@/app/components/_library-components/_library-listing';
 
 const LibraryContainer: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Library Page</Text>
+      <LibraryListing />
     </View>
   );
 };
