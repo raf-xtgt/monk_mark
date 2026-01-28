5d84dc5d2a895bbd5aa72ad5e9eb7cb651d4844a
raf-xtgt <rafaquat9905@gmail.com>
2026-01-28 20:33:21 +0600

    updated notebook content

--- backend/app/model/notebook/app_mm_notebook_content.py
+++ backend/app/model/notebook/app_mm_notebook_content.py
@@ -1,25 +1,3 @@
-"""
-CREATE TABLE IF NOT EXISTS app_mm_notebook_content (
-    guid UUID PRIMARY KEY,
-    user_guid UUID,
-    notebook_hdr_guid UUID,
-    library_hdr_guid UUID,
-    content_text TEXT,
-    image_url TEXT,
-    highlight_metadata JSON,
-    sequence_no SERIAL,
-    created_date TIMESTAMPTZ DEFAULT NOW(),
-    updated_date TIMESTAMPTZ DEFAULT NOW()
-);
-
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_text ON app_mm_notebook_content USING BTREE (content_text);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_sequence_no ON app_mm_notebook_content USING BTREE (sequence_no);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_created_date ON app_mm_notebook_content USING BTREE (created_date);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_updated_date ON app_mm_notebook_content USING BTREE (updated_date);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_user_guid ON app_mm_notebook_content USING BTREE (user_guid);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_lib_guid ON app_mm_notebook_content USING BTREE (library_hdr_guid);
-CREATE INDEX IF NOT EXISTS idx_app_mm_notebook_content_hdr_guid ON app_mm_notebook_content USING BTREE (notebook_hdr_guid);
-"""
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
