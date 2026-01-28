1c26d494b401ff9451652e8e94e0375e7fe1ffd1
raf-xtgt <rafaquat9905@gmail.com>
2026-01-28 20:42:30 +0600

    added the camera icon on note taking view

--- frontend/monk-mark-app/app/components/_notebook-components/_note-taking-view.tsx
+++ frontend/monk-mark-app/app/components/_notebook-components/_note-taking-view.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import NoteTakingTimer from './_note-taking-timer';
 
 const NoteTakingView: React.FC = () => {
+  const handleCameraPress = () => {
+    console.log('Camera button pressed');
+    // TODO: Implement camera functionality
+  };
+
   return (
     <View style={styles.container}>
       <NoteTakingTimer />
       {/* Note taking content will go here */}
+
+      {/* Floating Camera Button */}
+      <TouchableOpacity style={styles.floatingButton} onPress={handleCameraPress}>
+        <Ionicons name="camera" size={28} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,6 +25,22 @@ const NoteTakingView: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#6200ee',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8,
   },
 });
 
